cbeaa252487b489529a8a162dafd7fa7f8f87370
michaeltina <michaeltinashe00@gmail.com>
2026-04-17 09:03:57 +0200

    Add the hovering effect

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById('container')
+
+for (let row = 0; row < 16; row ++) {
+const row = document.createElement('div');
+row.classList.add('row');
+
+    for (let column = 0; column < 16; column ++){
+        const square = document.createElement('div');
+        square.classList.add("square");
+
+        row.append(square);
+    }
+container.append(row)
+}
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'lightgreen';
+    })
+})
